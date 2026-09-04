b945c64e7d1094615e6f497afd608bee3f299b73
malik5878714 <malik5878714@gmail.com>
2026-09-04 19:42:37 +0500

    Add files via upload

---
+++ Ayush/commonds/wednesday.js
@@ -0,0 +1,26 @@
+const fs = require("fs");
+module.exports.config = {
+	name: "wednesday",
+    version: "1.0.1",
+	hasPermssion: 0,
+	credits: "𝐄𝐌𝐌𝐀 𝐎𝐋𝐈𝐕𝐈𝐀", 
+	description: "hihihihi",
+	commandCategory: "no prefix",
+	usages: "wednesday",
+    cooldowns: 5, 
+};
+
+module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
+	var { threadID, messageID } = event;
+	if (event.body.indexOf("Wednesday")==0 || event.body.indexOf("wednesday")==0 || event.body.indexOf("wednesday")==0 || event.body.indexOf("wednesday")==0) {
+		var msg = {
+				body: "Wednesday 🧛🏻‍♀️",
+				attachment: fs.createReadStream(__dirname + `/noprefix/wednesday.mp4`)
+			}
+			api.sendMessage(msg, threadID, messageID);
+    api.setMessageReaction("🧛🏻‍♀️", event.messageID, (err) => {}, true)
+		}
+	}
+	module.exports.run = function({ api, event, client, __GLOBAL }) {
+
+  }
